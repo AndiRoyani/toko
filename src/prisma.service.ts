d9b623dc33d5e3339a02914934e6a5f93bb526a8
AndiRoyani <andiroyani@smkwikrama.sch.id>
2026-05-10 12:31:35 +0700

    first commit2

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,17 +1,29 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService implements OnModuleInit {
+  private client: any;
+
   constructor() {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const { PrismaClient } = require('@prisma/client');
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL,
     });
-    super({ adapter });
+    this.client = new PrismaClient({ adapter });
   }
 
   async onModuleInit() {
-    await this.$connect();
+    await this.client.$connect();
+  }
+
+  get user() { return this.client.user; }
+  get barang() { return this.client.barang; }
+  get kategori() { return this.client.kategori; }
+  get transaksi() { return this.client.transaksi; }
+  get detailTransaksi() { return this.client.detailTransaksi; }
+  $transaction(...args: any[]) {
+    return this.client.$transaction(...args);
   }
 }
